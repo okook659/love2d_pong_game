b192be725bb6af3737b28956d178390a27592434
okook659 <araulbora@gmail.com>
2025-05-30 00:30:52 +0000

    --feature:faire bouger aléatoirement la balle

--- main.lua
+++ main.lua
@@ -11,11 +11,23 @@ function love.load()
     print(height)
     love.window.setMode(width, height)
     ball = {}
-    ball.x = 300
-    ball.y = 200
-    ball.speed = 10
     ball.sprite = love.graphics.newImage('assets/images/arts/Ball.png')
     ball.width = ball.sprite:getWidth()
+    ball.x = 200
+    ball.y = 300
+    ball.speed = 200
+    ball.xvel = 0
+    ball.yvel = 0
+    if (0 < ball.x and ball.x < width) and ((0 < ball.y) and (ball.y< height)) then
+        if (love.math.random(2) == 1) then
+            ball.xvel = ball.speed
+            ball.yvel = ball.speed
+        end
+        if (love.math.random(2) == -1) then
+            ball.xvel = -ball.speed
+            ball.yvel = -ball.speed
+        end
+    end
 end
 
 function love.update(dt)
@@ -25,10 +37,12 @@ function love.update(dt)
     if player.y < (height - player.height) and love.keyboard.isDown("down") then
         player.y = player.y + player.speed
     end
+    ball.x = ball.x + ball.xvel*dt
+    ball.y = ball.y + ball.yvel*dt
 end
 
 function love.draw()
     love.graphics.draw(background, 0, 0)
-    love.graphics.draw(ball.sprite,( width - ball.width)/2, height/2)
+    love.graphics.draw(ball.sprite, ball.x, ball.y)
     love.graphics.draw(player.sprite, player.x, player.y)
 end
